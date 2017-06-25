b5391752e8e0f8f3b6b842a92d7f278958c55b6d
lpmayos <lpmayos@gmail.com>
2017-06-25 17:30:08 +0200

    changed .sh scripts parameters names to clarify use

--- launch_experiments_all.sh
+++ launch_experiments_all.sh
@@ -6,10 +6,12 @@ solr_sensembed_path="http://localhost:8983/solr/sensembed_vectors"
 path_to_train_val_videodatainfo="/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo.json"
 
 # Create video captions and compute similarities
-# python experiments.py -p $pickle_folder -e create_video_captions -s 0 -l 7010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo
-# python experiments.py -p $pickle_folder -e compute_similarities -s 0 -l 7010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo
+# python experiments.py --pickle_folder $pickle_folder --experiment create_video_captions --first 0 --last 7010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo
+# python experiments.py --pickle_folder $pickle_folder --experiment compute_similarities --first 0 --last 7010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo
 
 # experiments on videos 3000 to 3009, verbose
-python experiments.py -p $pickle_folder -e experiment1 -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo > results/experiment1/experiment1_small.txt
-python experiments.py -p $pickle_folder -e experiment2 -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo > results/experiment2/experiment2_small.txt
-python experiments.py -p $pickle_folder -e experiment3 -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo > results/experiment3/experiment3_small.txt
+# python experiments.py --pickle_folder $pickle_folder --experiment experiment1 --first 0 --last 7010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment1/experiment1.txt
+# python experiments.py --pickle_folder $pickle_folder --experiment experiment2 --first 0 --last 7010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment2/experiment2.txt
+# python experiments.py --pickle_folder $pickle_folder --experiment experiment3 --first 0 --last 7010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment3/experiment3.txt
+
+python experiments.py --pickle_folder $pickle_folder --experiment experiment3 --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment3/experiment3_all_reduced.txt
--- launch_experiments_small.sh
+++ launch_experiments_small.sh
@@ -6,10 +6,10 @@ solr_sensembed_path="http://localhost:8983/solr/sensembed_vectors"
 path_to_train_val_videodatainfo="/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo.json"
 
 # Create video captions and compute similarities
-# python experiments.py -p $pickle_folder -e create_video_captions -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo
-# python experiments.py -p $pickle_folder -e compute_similarities -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo
+# python experiments.py --pickle_folder $pickle_folder --experiment create_video_captions --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo
+# python experiments.py --pickle_folder $pickle_folder --experiment compute_similarities --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo
 
 # experiments on videos 3000 to 3009, verbose
-# python experiments.py -p $pickle_folder -e experiment1 -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo > results/experiment1/experiment1_small.txt
-# python experiments.py -p $pickle_folder -e experiment2 -s 3000 -l 3010 -v true -sp $solr_sensembed_path -jp $path_to_train_val_videodatainfo > results/experiment2/experiment2_small.txt
-python experiments.py -p $pickle_folder -e experiment3 -s 3000 -l 3010 -v true -x $solr_sensembed_path -y $path_to_train_val_videodatainfo > results/experiment3/experiment3_small.txt
+# python experiments.py --pickle_folder $pickle_folder --experiment experiment1 --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment1/experiment1_small.txt
+# python experiments.py --pickle_folder $pickle_folder --experiment experiment2 --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment2/experiment2_small.txt
+python experiments.py --pickle_folder $pickle_folder --experiment experiment3 --first 3000 --last 3010 -v true --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo > results/experiment3/experiment3_small.txt
