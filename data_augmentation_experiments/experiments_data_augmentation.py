044471e36dcf466521fbe832f60f297f4ca6ecd7
lpmayos <lpmayos@gmail.com>
2017-08-03 18:51:02 +0200

    similarity experiments with subject matching

--- data_augmentation_experiments/experiments_data_augmentation.py
+++ data_augmentation_experiments/experiments_data_augmentation.py
@@ -52,48 +52,56 @@ def prova():
     print ' '.join([a.form for a in predicate]) + ', ROOT: ' + [a.form for a in predicate if a.predicate_root][0] + '\n'
 
 
-# def combine_subjects_and_predicates_OLD():
-#     """ generates new training sentences by replacing tokens with synonyms
-#     """
-#     parser_en = TransitionClient(EN_PARSER)
+def compute_similarity(caption1_subject_tokens, caption2_subject_tokens):
+    """ Based on experiments_ranking > experiment4
+    """
+    similarities = []
+    for token1_id in caption1_subject_tokens:
 
-#     for video_id in range(config.first_video, config.last_video):
-#         video_captions = load_video_captions(video_id)
-#         subjects = []
-#         predicates = []
-#         for i, sentence in enumerate(video_captions.sentences):
-#             sentence_conll = parser_en.parse_text(sentence.sentence)
-#             try:
-#                 subject, predicate = sentence_conll.sentences[0].get_subject_and_predicate()
-#                 # print sentence.sentence
-#                 # print ' '.join([a.form for a in subject]) + ', ROOT: ' + [a.form for a in subject if a.subject_root][0]
-#                 # print ' '.join([a.form for a in predicate]) + ', ROOT: ' + [a.form for a in predicate if a.predicate_root][0] + '\n'
-#                 subj_singular = 'number=SG' in [a for a in subject if a.subject_root][0].pfeat
-#                 subject_text = ' '.join([a.form for a in subject])
-#                 predicate_text = ' '.join([a.form for a in predicate])
-#                 if subject_text not in [subj['text'] for subj in subjects]:
-#                     subjects.append({'subject': subject, 'text': subject_text, 'singular': subj_singular})
-#                 if predicate_text not in [pred['text'] for pred in predicates]:
-#                     predicates.append({'predicate': predicate, 'text': predicate_text, 'singular': subj_singular})  # NOTICE we mark predicate as singula or plural according to subject, not according to predicate because the model is noisy (i.e. classifies as plural 'are')
-#             except IndexError:
-#                 # print '\n[ERROR] Unable to extract subject/predicate from: ', sentence.sentence
-#                 pass
+        # find most similar token to sentence1.token1 in sentence2.tokens
+        most_similar_token_in_sentence = (None, float('-inf'))
+        for token2_id in caption2_subject_tokens:
+            if (token1_id, token2_id) in config.tokens_set.tokens_similarities_closest:
+                similarity = config.tokens_set.tokens_similarities_closest[(token1_id, token2_id)]
+                if similarity > most_similar_token_in_sentence[1]:
+                    most_similar_token_in_sentence = (token2_id, similarity)
 
-#         print '\n ------------------------------------------ video ' + str(video_id)
+        # store token similarity (depending on the experiments we check if it is over threshold)
+        if most_similar_token_in_sentence[0] is not None:
+            if most_similar_token_in_sentence[1] > 0.11:  # ranking exp > exp4 symmetrical th1
+                similarities.append((most_similar_token_in_sentence[0], 1.0))  # for each token we add 1 instead of similarity
+            else:
+                similarities.append((None, 0))
 
-#         print '\n@@@ Original sentences @@@'
-#         for sentence in video_captions.sentences:
-#             print sentence.sentence
+    # compute and store similarity between sentence1 and sentence2
+    if len(similarities) > 0:
+        similarity = float(sum([a[1] for a in similarities])) / len(similarities)
+    else:
+        similarity = 0
 
-#         print '\n@@@ Combined sentences @@@'
-#         for subj_pred in list(itertools.product(subjects, predicates)):
-#             subject = subj_pred[0]
-#             predicate = subj_pred[1]
-#             if subject['singular'] == predicate['singular']:
-#                 sentence = ' '.join([a.form for a in subject['subject']]) + ' ' + ' '.join([a.form for a in predicate['predicate']])
-#                 print sentence
+    return similarity
 
-#     # add_training_sentences(new_training_sentences, '/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo_synonyms.json')
+
+def senses_match(caption1, caption2):
+    """ If both subjects are talking about the same subject, both sentences are
+    good pair to exchange subject and predicate.
+    """
+    tokens_caption1 = caption1['sentence'].tokens_id_list
+    tokens_caption2 = caption2['sentence'].tokens_id_list
+
+    caption1_subject_tokens = [a for a in tokens_caption1 if config.tokens_set.get_token_by_id(a).token in caption1['subject']['text']]
+    caption2_subject_tokens = [a for a in tokens_caption2 if config.tokens_set.get_token_by_id(a).token in caption2['subject']['text']]
+
+    similarity1 = compute_similarity(caption1_subject_tokens, caption2_subject_tokens)
+    similarity2 = compute_similarity(caption2_subject_tokens, caption1_subject_tokens)
+    similarity = (similarity1 + similarity2) / 2
+
+    if similarity > 0.435:  # ranking exp > exp4 symmetrical th2
+        # print caption1['subject']['text'] + ' ------ IS SIMILAR TO ------ ' + caption2['subject']['text'] + ' (' + str(similarity) + ')'
+        return True
+    # print caption1['subject']['text'] + ' ------ IS NOT SIMILAR TO ------ ' + caption2['subject']['text'] + ' (' + str(similarity) + ')'
+    return False
+
 
 def combine_subjects_and_predicates():
     """ generates new training sentences by replacing tokens with synonyms
@@ -146,7 +154,7 @@ def combine_subjects_and_predicates():
             subject = caption['subject']
             for caption2 in parsed_captions:
                 number_matching = subject['singular'] == caption2['predicate']['singular']
-                sense_matching = True  # TODO lpmayos xxx
+                sense_matching = senses_match(caption, caption2)  # TODO lpmayos: put on True to execute first experiment
 
                 candidate_caption = caption['subject']['text'] + ' ' + caption2['predicate']['text']
                 if number_matching and sense_matching and candidate_caption not in current_captions:
@@ -157,7 +165,7 @@ def combine_subjects_and_predicates():
         #     print caption
         videos_new_captions[video_id] = list(set(new_captions))
 
-    add_training_sentences(videos_new_captions, '/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo_sub_pred_combinations.json')
+    add_training_sentences(videos_new_captions, '/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo_sub_pred_combinations_with_subject_matching.json')
 
 
 def main():
--- data_augmentation_experiments/launch_experiments.sh
+++ data_augmentation_experiments/launch_experiments.sh
@@ -6,8 +6,7 @@ pickle_folder="../pickle"
 solr_sensembed_path="http://localhost:8983/solr/sensembed_vectors"
 path_to_train_val_videodatainfo="/home/lpmayos/code/caption-guided-saliency/DATA/MSR-VTT/train_val_videodatainfo.json"
 first=0  #  0,      0
-last=7010  #    7010,   10
+last=10  #    7010,   10
 verbose='true'
-create_new_training_sentences='true'
 
 python experiments_data_augmentation.py --experiment 'combine_subjects_and_predicates' --pickle_folder $pickle_folder --first $first --last $last -v $verbose --solr_sensembed_path $solr_sensembed_path --path_to_train_val_videodatainfo $path_to_train_val_videodatainfo
